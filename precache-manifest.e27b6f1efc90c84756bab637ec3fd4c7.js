cadc085da2d2ed8f296229004f46ea8702496714
harshkumarraj <hraj9503@gmail.com>
2022-09-20 12:51:54 +0530

    Updates

---
+++ precache-manifest.e27b6f1efc90c84756bab637ec3fd4c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd21e36fad2c2064e437279fef654279",
+    "url": "/Text-Analyer/index.html"
+  },
+  {
+    "revision": "a558d5882aa3ddccba46",
+    "url": "/Text-Analyer/static/css/main.88888718.chunk.css"
+  },
+  {
+    "revision": "415bc81296ef4609dbcd",
+    "url": "/Text-Analyer/static/js/2.823a7877.chunk.js"
+  },
+  {
+    "revision": "a558d5882aa3ddccba46",
+    "url": "/Text-Analyer/static/js/main.40dd7968.chunk.js"
+  },
+  {
+    "revision": "9ad85fc7cdd127c2477c",
+    "url": "/Text-Analyer/static/js/runtime~main.d07bd48b.js"
+  }
+]);
